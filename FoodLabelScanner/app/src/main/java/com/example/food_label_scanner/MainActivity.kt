68902fd056eb5272ed0f586429abd3c8fa6540d4
Turcuș Adrian <adrian.turcus@student.upt.ro>
2024-11-14 12:51:03 +0200

    Added navigation drawer functionality using Jetpack Compose

--- FoodLabelScanner/app/src/main/java/com/example/food_label_scanner/MainActivity.kt
+++ FoodLabelScanner/app/src/main/java/com/example/food_label_scanner/MainActivity.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.food_label_scanner.ui.theme.FoodLabelScannerTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +32,63 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FoodLabelScannerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Screen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Screen(modifier: Modifier = Modifier){
+
+    val drawerState = rememberDrawerState( initialValue = DrawerValue.Closed)
+
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                DrawerContent()
+            }
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopBar(
+                    onOpenDrawer = {
+                        scope.launch { drawerState.apply { if (isClosed) open() else close() } }
+                    }
+                )
+            }
+        ){ padding ->
+            ScreenContent(modifier = Modifier.padding(padding))
+        }
+    }
+}
+
+@Composable
+fun DrawerContent(modifier: Modifier = Modifier){
+
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier){
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    onOpenDrawer: () -> Unit
+){
+    TopAppBar(navigationIcon = {
+        Icon(
+            imageVector = Icons.Default.Menu,
+            contentDescription = "Menu",
+            modifier = Modifier.clickable { onOpenDrawer() }
+        ) },
+        title = { Text(text = "Screen Name") }
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FoodLabelScannerTheme {
-        Greeting("Android")
-    }
-}
