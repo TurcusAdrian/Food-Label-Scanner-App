0eadfdd85b2db45dbd257197ecdd5d8cf9ce53da
Turcuș Adrian <adrian.turcus@student.upt.ro>
2024-11-18 17:44:42 +0200

    Added bottom bar - still to be worked on

--- FoodLabelScanner/app/src/main/java/com/example/food_label_scanner/MainActivity.kt
+++ FoodLabelScanner/app/src/main/java/com/example/food_label_scanner/MainActivity.kt
@@ -4,38 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Share
-import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.food_label_scanner.ui.theme.FoodLabelScannerTheme
@@ -53,13 +55,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
 @Composable
 fun Screen(modifier: Modifier = Modifier){
 
     val drawerState = rememberDrawerState( initialValue = DrawerValue.Closed)
 
     val scope = rememberCoroutineScope()
+
+    var selectedbutton by remember{
+        mutableIntStateOf(0)
+    }
 
     ModalNavigationDrawer(
         drawerState = drawerState,
@@ -70,6 +75,30 @@ fun Screen(modifier: Modifier = Modifier){
         }
     ) {
         Scaffold(
+
+            bottomBar = {
+                NavigationBar {
+                    BottomNavItems.forEachIndexed { index, bottomNavItem ->
+                        NavigationBarItem(
+                            selected = index == selectedbutton,
+                            onClick = {
+                                selectedbutton = index
+                            },
+                            icon = {
+                                Icon(
+                                    imageVector =
+                                    if(index == selectedbutton)
+                                        bottomNavItem.selectedIcon
+                                    else
+                                        bottomNavItem.unselectedIcon,
+                                    contentDescription = bottomNavItem.title)
+                            },
+                            label = {Text(text = bottomNavItem.title)}
+                        )
+                    }
+                }
+            },
+
             topBar = {
                 TopBar(
                     onOpenDrawer = {
@@ -84,7 +113,6 @@ fun Screen(modifier: Modifier = Modifier){
 }
 
 
-@Preview
 @Composable
 fun DrawerContent(modifier: Modifier = Modifier){
 
@@ -256,4 +284,49 @@ fun TopBar(
         ) }
     )
 }
+
+val BottomNavItems = listOf(
+    BottomNavItem(
+        title = "Home",
+        route = "Home",
+        selectedIcon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home
+    ),
+
+    BottomNavItem(
+        title = "Add Photos from Gallery" ,
+        route = "Add Photos from Gallery",
+        selectedIcon = Icons.Filled.Add,
+        unselectedIcon = Icons.Outlined.Add
+    ),
+
+    BottomNavItem(
+        title = "Take Picture",
+        route = "Take Picture",
+        selectedIcon = Icons.Filled.Call, //TO DO: change it to be camera
+        unselectedIcon = Icons.Outlined.Call
+    ),
+
+    BottomNavItem(
+        title = "Search",
+        route = "Search",
+        selectedIcon = Icons.Filled.Search,
+        unselectedIcon = Icons.Outlined.Search
+    ),
+
+    BottomNavItem(
+        title = "Favourites Items",
+        route = "Favourites Items",
+        selectedIcon = Icons.Filled.Star,
+        unselectedIcon = Icons.Outlined.Star
+    )
+)
+
+
+data class BottomNavItem(
+    val title: String,
+    val route: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
 
